54d274a8661032b426c16844231624632c787ccd
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-26 14:28:38 +0430

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   after_save :update_posts_counter
 
+  validate :title, presence: true, length: { maximum: 250 }
+  validate :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def return_recent_comments
     comment.order(created_at: :desc).limit(5)
   end
