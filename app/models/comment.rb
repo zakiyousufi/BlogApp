28e68aa35dbb1364d731b82a6dfd31489cdeed75
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2022-11-26 17:11:28 +0430

    Create the test for App and install dependisis

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
-  after_save :update_comments_counter
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :comment_counter
 
   private
 
-  def update_comments_counter
-    post.increment!(:comments_counter)
+  def comment_counter
+    posts.increment!(:comments_counter)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  has_many :post, foreign_key: 'author_id'
-  has_many :comment, foreign_key: 'author_id'
-  has_many :like, foreign_key: 'author_id'
-
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def recent_posts
-    post.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
