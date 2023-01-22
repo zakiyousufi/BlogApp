59f5e460b7d790426b7bba4815394d134b39b882
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-01-22 21:39:13 +0430

    Run bundel Install to install the messing gems

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sprockets-rails'
 
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
+gem 'sqlite3', '~> 1.4.0'
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_11_173355) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
@@ -27,8 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_173355) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
@@ -38,7 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_173355) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.integer "author_id", null: false
     t.integer "comments_count"
     t.integer "likes_count"
     t.index ["author_id"], name: "index_posts_on_author_id"
