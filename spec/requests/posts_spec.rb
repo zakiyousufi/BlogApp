96370174df20963a8212c4ecc75a5c8fa35e2316
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-01-25 19:27:48 +0430

    Create (post_spec.rb) for test the request discribe Get show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe PostsController, type: :request do
       expect(response.body).to include('Here is the list of posts:')
     end
   end
+  describe 'GET show' do
+    before :each do
+      get '/users/index/posts/index'
+    end
+
+    it 'renders show' do
+      expect(response).to render_template('posts/show')
+    end
+
+    it 'returns code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body has the right placeholder' do
+      expect(response.body).to include('Here is the list of posts for a given user:')
+    end
+  end
 end
