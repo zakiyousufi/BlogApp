a3d55efe407bb683c29d59c11982994b5a7b77c9
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-01-25 19:30:04 +0430

    Create (user_spec.rb) for test the request discribe Get show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,21 @@ RSpec.describe UsersController, type: :request do
       expect(response.body).to include('Here is the list of users:')
     end
   end
+  describe 'GET show' do
+    before :each do
+      get '/users/index'
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template('users/show')
+    end
+
+    it 'returns code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'response body has the right placeholder' do
+      expect(response.body).to include('Here is the user you are looking for:')
+    end
+  end
 end
